aa3a0a74e30ebfa55564fcc92a6a6db0bc1bac2e
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2020-12-19 06:59:03 +0100

    Created Song routes and table

---
+++ server/src/migrations/20201217180109-create-song.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Songs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      artist: {
+        type: Sequelize.STRING
+      },
+      genre: {
+        type: Sequelize.STRING
+      },
+      album: {
+        type: Sequelize.STRING
+      },
+      albumImageUrl: {
+        type: Sequelize.STRING
+      },
+      youtubeId: {
+        type: Sequelize.STRING
+      },
+      lyrics: {
+        type: Sequelize.TEXT
+      },
+      tab: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Songs');
+  }
+};
